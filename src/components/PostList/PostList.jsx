ff5b3a143416992888009e2a504318f3331a0616
Erk <61740386+pntErk@users.noreply.github.com>
2023-10-07 16:36:19 +0700

    Update PostList.jsx

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,15 +1,18 @@
 /* eslint-disable no-undef */
 import { useEffect, useState } from "react";
 import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
-import CommentForm from "../CommentForm";
+import CommentForm from "../CommentForm/CommentForm";
 // eslint-disable-next-line no-unused-vars
 import { firestore } from "../../firebase/firebase";
 import "./PostList.css";
-import HeartIcon from '../../assets/heart-regular.svg';
-import CommentIcon from '../../assets/comment-regular.svg'
+import IconHeartCircle from "../Icons/IconHeartCircle"
+import IconComment from "../Icons/IconComment";
+import CommentList from "../CommentList/CommentList";
+
 
 function PostList() {
   const [posts, setPosts] = useState([]);
+  const [openCommentForms, setOpenCommentForms] = useState([]); // State เก็บข้อมูลเกี่ยวกับการเปิด Comment Form
 
   useEffect(() => {
     // สร้างคิวรี่เพื่อเรียกข้อมูลโพสต์แบบเรียงตาม createdAt
@@ -22,6 +25,8 @@ function PostList() {
         ...doc.data(),
       }));
       setPosts(postList);
+      // สร้างอาเรย์ใหม่ที่มีขนาดเท่ากับจำนวนโพสต์ และกำหนดให้ค่าเริ่มต้นเป็น false สำหรับแต่ละโพสต์
+      setOpenCommentForms(Array(postList.length).fill(false));
     });
 
     return () => {
@@ -30,10 +35,17 @@ function PostList() {
     };
   }, []);
 
+  const toggleCommentForm = (index) => {
+    // สลับค่าในอาเรย์ openCommentForms ตาม index ที่รับเข้ามา
+    const newOpenCommentForms = [...openCommentForms];
+    newOpenCommentForms[index] = !newOpenCommentForms[index];
+    setOpenCommentForms(newOpenCommentForms);
+  };
+
   return (
-      <ul style={{ paddingLeft: '0px' }}>
-        {posts.map((post) => (
-          <li key={post.id}>
+      <ul style={{ paddingLeft: '0px' }} id="comment-ul">
+        {posts.map((post, index) => (
+          <li key={post.id} id="post-list">
             <div className="post-profiles">
                 <img src={"/moutain-and-sky-from-pov-2d.png"} alt="รูปภาพตัวอย่าง" />
                 <div className="user-details"><h1>Name</h1>
@@ -41,14 +53,23 @@ function PostList() {
             </div>
             <p>{post.content}</p>
             <div className="interact-icons-section">
-            <button>
-              <img src={HeartIcon} alt="Heart Icon" width={'36.8px'}/>
-              <label>LIKE</label></button>
               <button>
-              <img src={CommentIcon} alt="Comment Icon" width={'36.8px'}/>
-              <label>COMMENT</label></button>
+                <IconHeartCircle width={'38.8px'} height={'36.8px'}/>
+                <label>LIKE</label>
+              </button>
+              {/* เรียกใช้งานฟังก์ชัน toggleCommentForm เมื่อคลิกปุ่ม Comment */}
+              <button onClick={() => toggleCommentForm(index)}>
+                <IconComment width={'36.8px'} height={'36.8px'}/>
+                <label>COMMENT</label>
+              </button>
             </div>
-            <CommentForm postId={post.id} firestore={firestore} />
+            {/* แสดง CommentForm และ CommentList ถ้า openCommentForms[index] เป็น true */}
+            {openCommentForms[index] && (
+              <div>
+                <CommentForm postId={post.id} firestore={firestore} />
+              </div>
+            )}
+            <CommentList postId={post.id} />
           </li>
         ))}
       </ul>
