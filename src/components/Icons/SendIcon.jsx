35ed32e8a62de11fd84b46e0122f30ccddaf70c2
Erk <61740386+pntErk@users.noreply.github.com>
2023-10-07 16:36:10 +0700

    Create SendIcon.jsx

---
+++ src/components/Icons/SendIcon.jsx
@@ -0,0 +1,19 @@
+// icon:send | Zondicons http://www.zondicons.com | Steve Schoger
+// eslint-disable-next-line no-unused-vars
+import * as React from "react";
+
+function IconSend(props) {
+  return (
+    <svg
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M0 0l20 10L0 20V0zm0 8v4l10-2L0 8z" />
+    </svg>
+  );
+}
+
+export default IconSend;
