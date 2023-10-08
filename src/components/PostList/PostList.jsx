68fa5e6715119c7574fef91c15c26cb2ff8d4426
Erk <61740386+pntErk@users.noreply.github.com>
2023-10-08 12:50:11 +0700

    Update PostList.jsx
    refactor code

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
 import CommentForm from "../CommentForm/CommentForm";
@@ -7,12 +8,45 @@ import IconHeartCircle from "../Icons/IconHeartCircle";
 import IconComment from "../Icons/IconComment";
 import CommentList from "../CommentList/CommentList";
 
+function PostListItem({ post, index, openCommentForms, toggleCommentForm }) {
+  return (
+    <li key={post.id} id="post-list">
+      <div className="post-profiles">
+        <img
+          src={"/moutain-and-sky-from-pov-2d.png"}
+          alt="รูปภาพตัวอย่าง"
+        />
+        <div className="user-details">
+          <h1>Name</h1>
+          <h2>{post.createdAt.toDate().toLocaleString()}</h2>
+        </div>
+      </div>
+      <p>{post.content}</p>
+      <div className="interact-icons-section">
+        <button>
+          <IconHeartCircle width={"38.8px"} height={"36.8px"} />
+          <label>LIKE</label>
+        </button>
+        <button onClick={() => toggleCommentForm(index)}>
+          <IconComment width={"36.8px"} height={"36.8px"} />
+          <label>COMMENT</label>
+        </button>
+      </div>
+      {openCommentForms[index] && (
+        <div className="comment-form active">
+          <CommentForm postId={post.id} firestore={firestore} className="comment-form" />
+        </div>
+      )}
+      <CommentList postId={post.id} />
+    </li>
+  );
+}
+
 function PostList() {
   const [posts, setPosts] = useState([]);
-  const [openCommentForms, setOpenCommentForms] = useState([]); // State เก็บข้อมูลเกี่ยวกับการเปิด Comment Form
+  const [openCommentForms, setOpenCommentForms] = useState([]);
 
   useEffect(() => {
-    // สร้างคิวรี่เพื่อเรียกข้อมูลโพสต์แบบเรียงตาม createdAt
     const postsRef = collection(firestore, "posts");
     const q = query(postsRef, orderBy("createdAt", "desc"));
 
@@ -22,18 +56,15 @@ function PostList() {
         ...doc.data(),
       }));
       setPosts(postList);
-      // สร้างอาเรย์ใหม่ที่มีขนาดเท่ากับจำนวนโพสต์ และกำหนดให้ค่าเริ่มต้นเป็น false สำหรับแต่ละโพสต์
       setOpenCommentForms(Array(postList.length).fill(false));
     });
 
     return () => {
-      // ยกเลิกการสมัครสมาชิกเมื่อคอมโพเนนต์ถูกยกเลิก
       unsubscribe();
     };
   }, []);
 
   const toggleCommentForm = (index) => {
-    // สลับค่าในอาเรย์ openCommentForms ตาม index ที่รับเข้ามา
     const newOpenCommentForms = [...openCommentForms];
     newOpenCommentForms[index] = !newOpenCommentForms[index];
     setOpenCommentForms(newOpenCommentForms);
@@ -42,41 +73,13 @@ function PostList() {
   return (
     <ul style={{ paddingLeft: "0px" }} id="comment-ul">
       {posts.map((post, index) => (
-        <li key={post.id} id="post-list">
-          <div className="post-profiles">
-            <img
-              src={"/moutain-and-sky-from-pov-2d.png"}
-              alt="รูปภาพตัวอย่าง"
-            />
-            <div className="user-details">
-              <h1>Name</h1>
-              <h2>{post.createdAt.toDate().toLocaleString()}</h2>
-            </div>
-          </div>
-          <p>{post.content}</p>
-          <div className="interact-icons-section">
-            <button>
-              <IconHeartCircle width={"38.8px"} height={"36.8px"} />
-              <label>LIKE</label>
-            </button>
-            {/* เรียกใช้งานฟังก์ชัน toggleCommentForm เมื่อคลิกปุ่ม Comment */}
-            <button onClick={() => toggleCommentForm(index)}>
-              <IconComment width={"36.8px"} height={"36.8px"} />
-              <label>COMMENT</label>
-            </button>
-          </div>
-          {/* แสดง CommentForm และ CommentList ถ้า openCommentForms[index] เป็น true */}
-          {openCommentForms[index] && (
-            <div className="comment-form active">
-              <CommentForm
-                postId={post.id}
-                firestore={firestore}
-                className="comment-form"
-              />
-            </div>
-          )}
-          <CommentList postId={post.id} />
-        </li>
+        <PostListItem
+          key={post.id}
+          post={post}
+          index={index}
+          openCommentForms={openCommentForms}
+          toggleCommentForm={toggleCommentForm}
+        />
       ))}
     </ul>
   );
